352e791fda20d9147d37b5ecc0a965917a1a212f
Wynn Wilkes <wynnw@movenetworks.com>
2013-01-23 04:31:45 -0800

    add request pipelining support. pathoc gets a -l option to turn this on, and the Pathoc api gets two new api's for pipelining a series of requests.

--- libpathod/pathoc.py
+++ libpathod/pathoc.py
@@ -39,6 +39,26 @@ class Pathoc(tcp.TCPClient):
             except tcp.NetLibError, v:
                 raise PathocError(str(v))
 
+    def pipelined_requests(self, specs):
+        """
+            Return a list of (httpversion, code, msg, headers, content) tuples.
+            The requests defined by the list of spec are sent using http pipelining.
+
+            May raise language.ParseException, netlib.http.HttpError or
+            language.FileAccessDenied.
+        """
+        parsed_specs = {}  # spec -> r, ret
+        for spec in specs:
+            r = language.parse_request(self.settings, spec)
+            ret = language.serve(r, self.wfile, self.settings, self.host)
+            parsed_specs[spec] = r, ret
+        self.wfile.flush()
+        rval = []
+        for spec in specs:
+            r, ret = parsed_specs[spec]
+            rval.append(http.read_response(self.rfile, r.method, None))
+        return rval
+
     def request(self, spec):
         """
             Return an (httpversion, code, msg, headers, content) tuple.
@@ -123,3 +143,77 @@ class Pathoc(tcp.TCPClient):
                 if resp:
                     self._show_summary(fp, *resp)
             return True
+
+
+    def print_pipelined_requests(self, specs, showreq, showresp, explain, hexdump, ignorecodes, ignoretimeout, fp=sys.stdout):
+        """
+            Performs a series of pipelined requests, and prints results to the specified
+            file descriptor.
+
+            spec: A request specification
+            showreq: Print requests
+            showresp: Print responses
+            explain: Print request explanation
+            hexdump: When printing requests or responses, use hex dump output
+            ignorecodes: Sequence of return codes to ignore
+
+            Returns True if we have a non-ignored response.
+        """
+        if explain:
+            r = r.freeze(self.settings, self.host)
+
+        if showreq:
+            self.wfile.start_log()
+        if showresp:
+            self.rfile.start_log()
+
+        # write all the requests
+        parsed_requests = {}  # spec -> r, req
+        for spec in specs:
+            try:
+                r = language.parse_request(self.settings, spec)
+            except language.ParseException, v:
+                print >> fp, "Error parsing request spec: %s"%v.msg
+                print >> fp, v.marked()
+                return
+            except language.FileAccessDenied, v:
+                print >> fp, "File access error: %s"%v
+                return
+
+            req = language.serve(r, self.wfile, self.settings, self.host)
+            self.wfile.flush()
+
+            parsed_requests[spec] = r, req
+
+        # read all the responses
+        for spec in specs:
+            resp = None
+            r, req = parsed_requests[spec]
+            try:
+                resp = http.read_response(self.rfile, r.method, None)
+            except http.HttpError, v:
+                print >> fp, "<< HTTP Error:", v.msg
+            except tcp.NetLibTimeout:
+                if ignoretimeout:
+                    continue
+                print >> fp, "<<", "Timeout"
+            except tcp.NetLibDisconnect: # pragma: nocover
+                print >> fp, "<<", "Disconnect"
+
+            if req:
+                if ignorecodes and resp and resp[1] in ignorecodes:
+                    continue
+                if explain:
+                    print >> fp, ">> Spec:", r.spec()
+
+                if showreq:
+                    self._show(fp, ">> Request", self.wfile.get_log(), hexdump)
+
+                if showresp:
+                    self._show(fp, "<< Response", self.rfile.get_log(), hexdump)
+                else:
+                    if resp:
+                        self._show_summary(fp, *resp)
+
+        return True
+
